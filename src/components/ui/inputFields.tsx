a1870c9c7f26615c1114cd09a049c2001208ebd3
devgcr <devgcr@protonmail.com>
2025-09-02 10:41:39 +0300

    fix error formatting

--- src/components/ui/inputFields.tsx
+++ src/components/ui/inputFields.tsx
@@ -44,7 +44,7 @@ export default function InputField({
         />
       )}
       {error && (
-        <p className="mt-2 text-sm font-medium text-red-800 bg-red-50 px-3 py-2 rounded-md border border-red-200">
+        <p className="mt-2 text-sm font-semibold text-red-700 bg-red-100 px-3 py-2 rounded-md border border-red-300">
           {error}
         </p>
       )}
