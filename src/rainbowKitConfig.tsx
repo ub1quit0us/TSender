01854573b507548a40c327930175d22b0291649e
devgcr <devgcr@protonmail.com>
2025-09-08 10:50:31 +0300

    update wallet kit

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,32 +1,11 @@
-"use client"
 
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-// EDIT CHAINS BELOW
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-  anvil,
-  sonic
-} from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { mainnet, polygon, optimism, arbitrum, base, anvil, sonic } from 'wagmi/chains';
 
 export default getDefaultConfig({
-  // EDIT NAME OF THE APP BELOW
   appName: 'TSender',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  // EDIT CHAINS BELOW
-  // chains: [mainnet, polygon, optimism, arbitrum, base, anvil, sonic],
   chains: [mainnet, anvil],
-  ssr: false, // If your dApp uses server side rendering (SSR)
+  ssr: false
 });
