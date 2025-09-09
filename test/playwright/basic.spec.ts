dcd64b287c6df214f63c62c2ff89dc8a46cba744
devgcr <devgcr@protonmail.com>
2025-09-09 10:33:52 +0300

    basic synpress test

--- test/playwright/basic.spec.ts
+++ test/playwright/basic.spec.ts
@@ -1,20 +1,30 @@
-import { test, expect } from '@playwright/test';
+import basicSetup from '../wallet-setup/basic.setup'
+import { testWithSynpress } from '@synthetixio/synpress'
+import { MetaMask, metaMaskFixtures } from "@synthetixio/synpress/playwright"
 
+const test = testWithSynpress(metaMaskFixtures(basicSetup))
+const { expect } = test
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-
-  // Expect a title to contain a substring.
-
-  await expect(page).toHaveTitle(/TSender/);
-
+  await page.goto('http://localhost:3000/')
+  await expect(page).toHaveTitle(/TSender/)
 });
 
 
-test('should show the airdrop form when connected, otherwise not', async ({ page, context }) => {
-  // Navigate to app
+test('should show the airdrop form when connected, otherwise not', async ({ page, context, metamaskPage, extensionId }) => {
   await page.goto('http://localhost:3000/')
-
   await expect(page.getByText('Connect Wallet')).toBeVisible()
 
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId)
+  await page.getByTestId('rk-connect-button').click()
+  await page.getByTestId('rk-wallet-option-metaMask').waitFor({
+    state: 'visible',
+    timeout: 30000
+  })
+  await page.getByTestId('rk-wallet-option-metaMask').click()
+  await metamask.connectToDapp()
+
+  await expect(page.getByText("Token Address")).toBeVisible()
+  await expect(page.getByText("Recipients")).toBeVisible()
+  await expect(page.getByText("Amounts")).toBeVisible()
 });
